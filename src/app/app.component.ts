d23ff3619d7d5d67f62301574fb5de216797ad0a
Germangami <kontakt.herman@gmail.com>
2024-10-28 21:43:19 +0100

    feature/chewiCheck-1-create-main-layout create layout, header, footer components  and client page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainLayoutComponent } from "./layout/main-layout/main-layout.component";
+import { ClientPageComponent } from './components/client-page/client-page.component';
+import { DatePickerComponent } from './shared/date-picker/date-picker.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainLayoutComponent],
+  imports: [RouterOutlet, ClientPageComponent, DatePickerComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'ChewiCheck';
